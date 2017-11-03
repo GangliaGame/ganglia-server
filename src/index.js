3e88edd6d56ae6a195860a7a9623519516439878
Avi Romanoff <avi@romanoff.me>
2017-11-02 21:43:04 -0400

    gameState, weapons

--- src/index.js
+++ src/index.js
@@ -4,9 +4,44 @@ import type { $Request } from 'express';
 import express from 'express'
 const app = express()
 
-const port = process.env.PORT || 9000
+const NUM_BAYS = 2
+const PORTS_PER_BAY = 4 // also change SequenceComponent type
 
-type Sequence = Array<Array<'*' | number | null>>
+const weapons = {
+  Phasers: 'Phasers',
+  PulseBomb: 'Pulse Bomb',
+  DisruptorBeam: 'Disruptor Beam',
+}
+
+const combos: $ReadOnlyArray<Combo> = [
+  {
+    name: 'REBOOT',
+    sequence: [[0,1,null,null],[0,1,null,null]],
+  },
+  {
+    name: weapons.DisruptorBeam,
+    sequence: [[0,'*','*',3],['*','*','*','*']],
+  },
+  {
+    name: weapons.PulseBomb,
+    sequence: [[null,null,1,3],['*','*','*','*']],
+  },
+  {
+    name: weapons.Phasers,
+    sequence: [[3,2,null,null],['*','*','*','*']],
+  },
+]
+
+type Weapon = $Keys<typeof weapons>
+
+type ID = number
+
+type Enemy = {
+  x: number,
+  y: number,
+  kind: 'green' | 'orange' | 'purple',
+  id: ID,
+}
 
 type Port = {
   wire: ?number,
@@ -15,90 +50,96 @@ type Port = {
 
 type Bay = Array<Port>
 
+type SequenceComponent = 0 | 1 | 2 | 3 | '*' | null
+
+type Sequence = Array<Array<SequenceComponent>>
+
 type Combo = {
   name: string,
   sequence: Sequence,
 }
 
-app.use(function(req: $Request, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+type GameState = {
+  bays: Array<Bay>,
+  enemies: Array<Enemy>,
+  weapon: ?Weapon,
+  gameOver: boolean,
+  score: number,
+}
 
-const NUM_BAYS = 2
-const PORTS_PER_BAY = 4
+function newGameState(): GameState {
+  const makeBay = (size: number): Bay => Array(size).fill().map(() => ({
+  	wire: null,
+  	isOnline: true,
+  }))
+  return {
+    bays: Array(NUM_BAYS).fill().map(() => makeBay(PORTS_PER_BAY)),
+    enemies: [],
+    weapon: null,
+    gameOver: false,
+    score: 0,
+  }
+}
 
-const makeBay = (size : number): Bay => Array(size).fill().map((_, i): Port => ({
-	wire: null,
-	isOnline: true,
-}))
+function activeCombos(bays: Array<Bay>, combos: Array<Combo>) {
+  const componentMatches = ([a, b]) => (a === '*') || (b === '*') || (a === b)
+  const sequenceMatches = (A, B) => _.zip(_.flatten(A), _.flatten(B)).every(componentMatches)
+  const baysToSequence = bays => bays.map(bay => bay.map(({wire}) => wire))
 
-let bays: Array<Bay> = Array(NUM_BAYS).fill().map(() => makeBay(4))
-
-// null => no wire present
-// undefined => wildcard
-// n => wire n
-let combos: Array<Combo> = [
-  {
-    name: 'REBOOT',
-    sequence: [[0,1,null,null],[0,1,null,null]],
-  },
-  {
-    name: 'BOOST_SHIELDS',
-    sequence: [['*','*',2,3],['*','*','*','*']],
-  },
-  {
-    name: 'BOOST_SHIELDS',
-    sequence: [['*','*','*','*'],['*','*',2,3]],
-  },
-  {
-    name: 'BAY_0_EMPTY',
-    sequence: [[null,null,null,null],['*','*','*','*']],
-  },
-  {
-    name: 'PHASERS',
-    sequence: [[0,1,2,'*'],['*','*','*','*']],
-  },
-]
-
-const componentMatches = ([a, b]) => (a === '*') || (b === '*') || (a === b)
-const sequenceMatches = (A, B) => _.zip(_.flatten(A), _.flatten(B)).every(componentMatches)
-const baysToSequence = bays => bays.map(bay => bay.map(({wire}) => wire))
-const getCombos = (bays, combos) =>
-  combos
+  return combos
   .filter(({sequence}) => sequenceMatches(baysToSequence(bays), sequence))
   .map(({name}) => name)
+}
 
-const serializeState = (bays, combos) => ({ bays, combos: getCombos(bays, combos) })
-const sendStateAsJson = res => res.json(serializeState(bays, combos))
+const destroyEnemy = (id: ID, state: GameState): GameState => {
+  let newState = _.cloneDeep(state)
+  newState.enemies = state.enemies.filter(enemy => enemy.id !== id)
+  return newState
+}
 
 app.get('/connect/wire/:wire/port/:port/bay/:bay', (req: $Request, res) => {
 	const wire = Number(req.params.wire)
 	const bay = Number(req.params.bay)
 	const port = Number(req.params.port)
 	console.log(`connected wire ${wire} to port ${port} bay ${bay}`)
-	bays[bay][port].wire = wire
-	sendStateAsJson(res)
+	state.bays[bay][port].wire = wire
+	res.json(state)
 })
 
 app.get('/disconnect/port/:port/bay/:bay', (req: $Request, res) => {
 	const bay = Number(req.params.bay)
 	const port = Number(req.params.port)
-	const wire = bays[bay][port].wire
+	const wire = state.bays[bay][port].wire
 	if (wire === null) {
 		console.log(`WARNING: no wire is plugged in to ${port} bay ${bay}`)
 	} else {
 		console.log(`disconnected wire ${String(wire)} from port ${port} bay ${bay}`)
-		bays[bay][port].wire = null
+		state.bays[bay][port].wire = null
 	}
-	sendStateAsJson(res)
+	res.json(state)
 })
 
 app.get('/state', (req: $Request, res) => {
-	sendStateAsJson(res)
+	res.json(state)
 })
 
-app.listen(port, function () {
-  console.log(`Serving on port ${port} 🙃`)
+app.get('/destroy/enemy/:enemyId', (req: $Request, res) => {
+  const enemyId = Number(req.params.enemyId)
+  console.log(`destroyed enemy with id ${enemyId}`)
+  state = destroyEnemy(enemyId, state)
+  res.json(state)
 })
+
+// set CORS headers
+app.use((req: $Request, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+const port = process.env.PORT || 9000
+app.listen(port, () => {
+  console.log(`Serving on port ${port} 👾`)
+})
+
+let state = newGameState()
