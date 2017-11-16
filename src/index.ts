fd04a9b9f34635ebf6ef9a8e145c56d61ecde38a
Avi Romanoff <avi@romanoff.me>
2017-11-15 22:13:57 -0500

    Rewrite gameserver, do logic client-side

--- src/index.ts
+++ src/index.ts
@@ -1,185 +1,66 @@
-import * as _ from 'lodash'
+// import * as _ from 'lodash'
 import * as express from 'express'
 const app = express()
 
-const ENEMY_ADVANCE_TIME = 3000
-const POINTS_PER_ENEMY = 25
-const NUM_STARTING_ENEMIES = 4
-const GRID_SIZE = 15
-const NUM_BAYS = 2
-const PORTS_PER_BAY = 4
-
 type timestamp = number
-type seconds = number
+
+enum Color {
+  red = 1,
+  orange,
+  yellow,
+  green,
+  blue,
+  purple
+}
 
 type Weapon = {
-  name: string,
-  color: 'pink' | 'blue' | 'white' | 'black',
-  sequence: Sequence,
-  duration: seconds,
+  name: string
+  color: Color,
 }
 
 const weapons: Array<Weapon> = [
   {
     name: 'Xanar Shells',
-    sequence: [[3,'*','*',2],['*','*','*','*']],
-    duration: 10,
-    color: 'blue',
+    color: Color.red,
   },
   {
     name: 'Plasma Field',
-    sequence: [[3,null,0,null],['*','*','*','*']],
-    duration: 10,
-    color: 'pink',
+    color: Color.green,
   },
   {
     name: 'High-Energy Pulse Laser',
-    sequence: [[3,null,null,0],['*','*','*','*']],
-    duration: 10,
-    color: 'black',
+    color: Color.blue,
   },
   {
     name: 'Neutrino Torpedos',
-    sequence: [[3,null,null,1],['*','*','*','*']],
-    duration: 10,
-    color: 'white',
+    color: Color.yellow,
   },
 ]
 
-enum EnemyKind {
-  Blue = 'blue',
-  Pink = 'pink',
-  White = 'white',
-  Black = 'black',
+// function weaponOfColorName(name: keyof typeof Color): Weapon | undefined {
+//   return weapons.find(weapon => weapon.color === Color[name])
+// }
+
+function weaponOfColor(color: Color): Weapon | undefined {
+  return weapons.find(weapon => weapon.color === color)
 }
-
-type EnemyID = number
-
-type Enemy = {
-  x: number,
-  y: number,
-  kind: string,
-  id: EnemyID,
-  isDestroyed: boolean,
-}
-
-type Port = {
-  wire?: number,
-  isOnline: boolean,
-}
-
-type Bay = Array<Port>
-
-type SequenceComponent = number | '*' | null
-
-type Sequence = Array<Array<SequenceComponent>>
 
 type GameState = {
-  bays: Array<Bay>,
-  enemies: Array<Enemy>,
-  weaponStartTime?: timestamp,
-  weapon?: Weapon,
+  weapon: Weapon | null,
   gameOver: boolean,
   gameWon: boolean,
-  score: number,
   gameStarted: boolean,
   gameStartTime?: timestamp,
 }
 
 function newGameState(): GameState {
-  const makeBay = (size: number): Bay => _.range(size).map(() => ({
-  	wire: null,
-  	isOnline: true,
-  }))
-  const startingEnemies = _.times(NUM_STARTING_ENEMIES, i => newEnemy(i))
   return {
-    bays: _.range(NUM_BAYS).map(() => makeBay(PORTS_PER_BAY)),
-    enemies: startingEnemies,
     weapon: null,
-    weaponStartTime: null,
     gameOver: false,
     gameWon: false,
     gameStarted: false,
     gameStartTime: Date.now(),
-    score: 0,
   }
-}
-
-function activeWeapon(bays: Array<Bay>, weapons: Array<Weapon>): Weapon | null {
-  const componentMatches = ([a, b]: [SequenceComponent, SequenceComponent]) => (a === '*') || (b === '*') || (a === b)
-  const sequenceMatches = (A: Sequence, B: Sequence) => _.zip(_.flatten(A), _.flatten(B)).every(componentMatches)
-  const baysToSequence = (bays: Array<Bay>) => bays.map(bay => bay.map(({wire}) => wire))
-  const weapon = weapons.find(({sequence}) => sequenceMatches(baysToSequence(bays), sequence))
-  // No weapon active
-  if (!weapon) {
-    return null
-  }
-  return weapon
-}
-
-function newEnemy(id: EnemyID): Enemy {
-  const randomXPosition: number = _.random(GRID_SIZE - 1)
-  const randomEnemyKind: string = _.sample(_.keys(EnemyKind))
-  return {
-    id,
-    x: randomXPosition,
-    y: 0,
-    kind: randomEnemyKind,
-    isDestroyed: false,
-  }
-}
-
-function advanceEnemies(state: GameState): GameState {
-  let newState = _.cloneDeep(state)
-  const advanceEnemy = (enemy: Enemy): Enemy => {
-    if (enemy.isDestroyed) return enemy
-    enemy.y = Math.min(enemy.y + 1, 14)
-    if (enemy.y === GRID_SIZE) {
-      state.gameOver = true
-    }
-    return enemy
-  }
-  newState.enemies = state.enemies.map(advanceEnemy)
-  return newState
-}
-
-function addEnemy(state: GameState): GameState {
-  let newState = _.cloneDeep(state)
-  const nextEnemyId: EnemyID = _.max(_.map(state.enemies, 'id')) + 1
-  newState.enemies = _.concat(state.enemies, newEnemy(nextEnemyId))
-  return newState
-}
-
-function gameTick(state: GameState) {
-  if (!state.gameStarted || state.gameOver) return;
-  if (state.weaponStartTime && state.weapon) {
-    const weaponMillisRemaining = Date.now() - state.weaponStartTime
-    const didWeaponExpire = weaponMillisRemaining > state.weapon.duration * 1000
-    if (didWeaponExpire) {
-      state.weapon = null
-      state.weaponStartTime = null
-    }
-  }
-}
-
-function gameAdvanceTick(state: GameState) {
-  if (!state.gameStarted || state.gameOver) return
-  if (state.gameStartTime) {
-    advanceEnemies(state)
-  }
-}
-
-function destroyEnemy(id: EnemyID, state: GameState): GameState {
-  let newState = _.cloneDeep(state)
-  newState.enemies = state.enemies.map(enemy => {
-    if (enemy.id === id) {
-      enemy.isDestroyed = true
-      state.score += POINTS_PER_ENEMY
-    }
-    return enemy
-  })
-  newState.gameWon = newState.enemies.every(enemy => enemy.isDestroyed)
-  return newState
 }
 
 // set CORS headers
@@ -190,43 +71,17 @@ app.use((req, res, next) => {
 })
 
 app.get('/state', (req, res) => {
-	res.json(state)
+  res.json(state)
 })
 
-app.get('/connect/wire/:wire/port/:port/bay/:bay', (req, res) => {
-	const wire = Number(req.params.wire)
-	const bay = Number(req.params.bay)
-	const port = Number(req.params.port)
-	console.log(`⭕️  Connected wire ${wire} to port ${port} bay ${bay}`)
-	state.bays[bay][port].wire = wire
-  if (!state.gameOver) {
-    state.gameStarted = true
-  }
-  state.weapon = activeWeapon(state.bays, weapons)
-  if (state.weapon) {
-    state.weaponStartTime = Date.now()
-  }
-	res.json(state)
+app.get('/weapon/disable', (req, res) => {
+  state.weapon = null
+  res.json(state)
 })
 
-app.get('/disconnect/port/:port/bay/:bay', (req, res) => {
-	const bay = Number(req.params.bay)
-	const port = Number(req.params.port)
-	const wire = state.bays[bay][port].wire
-	if (wire === null) {
-		console.log(`⚠️  WARNING: no wire is plugged in to ${port} bay ${bay}`)
-	} else {
-		console.log(`❌  Disconnected wire ${String(wire)} from port ${port} bay ${bay}`)
-		state.bays[bay][port].wire = null
-	}
-  state.weapon = activeWeapon(state.bays, weapons)
-	res.json(state)
-})
-
-app.get('/destroy/enemy/:enemyId', (req, res) => {
-  const enemyId = Number(req.params.enemyId)
-  console.log(`💥  Destroyed enemy with id ${enemyId}`)
-  state = destroyEnemy(enemyId, state)
+app.get('/weapon/enable/:colorName', (req, res) => {
+  const colorName: keyof typeof Color = req.params.colorName
+  state.weapon = weaponOfColor(Color[colorName]) || state.weapon
   res.json(state)
 })
 
@@ -241,6 +96,3 @@ app.listen(port, () => {
 })
 
 let state = newGameState()
-
-setInterval(() => gameTick(state), 10)
-setInterval(() => gameAdvanceTick(state), ENEMY_ADVANCE_TIME)
