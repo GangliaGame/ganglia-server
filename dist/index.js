a95aceb5f50c0bd407cdeb0b398c8c56477eae27
Avi Romanoff <avi@romanoff.me>
2017-11-27 19:20:27 -0500

    only socket.io

--- dist/index.js
+++ dist/index.js
@@ -6,68 +6,22 @@ const socketIo = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-function newGameState() {
-    return {
-        weaponLevel: 0,
-        isGameWon: false,
-        isGameLost: false,
-        isGameStarted: false,
-        isShieldActive: false,
-    };
-}
 // set CORS headers
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
-app.get('/state', (req, res) => {
-    res.json(state);
-});
-app.get('/weapon/set/:weaponLevel', (req, res) => {
-    const prevWeaponLevel = state.weaponLevel;
-    state.weaponLevel = Number(req.params.weaponLevel);
-    console.log(`🔫   weapon level ${prevWeaponLevel} -> ${state.weaponLevel}`);
-    res.json(state);
-});
-app.get('/shield/on', (req, res) => {
-    state.isShieldActive = true;
-    console.log(`🛡   shield -> on`);
-    res.json(state);
-});
-app.get('/shield/off', (req, res) => {
-    state.isShieldActive = false;
-    console.log(`🛡   shield -> off`);
-    res.json(state);
-});
-app.get('/game/new', (req, res) => {
-    state = newGameState();
-    res.json(state);
-});
-app.get('/game/start', (req, res) => {
-    state.isGameStarted = true;
-    res.json(state);
-});
-app.get('/game/lost', (req, res) => {
-    console.log(`☠️   game lost`);
-    state.isGameLost = true;
-    res.json(state);
-});
-app.get('/game/won', (req, res) => {
-    state.isGameWon = true;
-    res.json(state);
-});
 io.on('connection', socket => {
-    console.log('⚡️  a user connected');
-    const broadcastEvents = ['move:up', 'move:down', 'move:stop'];
-    broadcastEvents.map(event => socket.on(event, () => socket.broadcast.emit(event)));
+    console.log('⚡️ connected');
+    const broadcastEvents = ['move', 'shield', 'weapon', 'state'];
+    broadcastEvents.map(event => socket.on(event, data => socket.broadcast.emit(event, data)));
     socket.on('disconnect', () => {
-        console.log('🔌  a user disconnected');
+        console.log('🔌  disconnected');
     });
 });
 const port = process.env.PORT || 9000;
 server.listen(port, () => {
     console.log(`👾  Serving on port ${port}`);
 });
-let state = newGameState();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,6 @@ const app = express()
 const server = http.createServer(app)
 const io = socketIo(server)
 
-type WeaponLevel = 0 | 1 | 2 | 3
-
-type GameState = {
-  weaponLevel: WeaponLevel
-  isGameWon: boolean
-  isGameLost: boolean
-  isGameStarted: boolean
-  isShieldActive: boolean
-}
-
-function newGameState(): GameState {
-  return {
-    weaponLevel: 0,
-    isGameWon: false,
-    isGameLost: false,
-    isGameStarted: false,
-    isShieldActive: false,
-  }
-}
-
 // set CORS headers
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -32,58 +12,13 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/state', (req, res) => {
-  res.json(state)
-})
-
-app.get('/weapon/set/:weaponLevel', (req, res) => {
-  const prevWeaponLevel = state.weaponLevel
-  state.weaponLevel = Number(req.params.weaponLevel) as WeaponLevel
-  console.log(`🔫   weapon level ${prevWeaponLevel} -> ${state.weaponLevel}`)
-  res.json(state)
-})
-
-
-app.get('/shield/on', (req, res) => {
-  state.isShieldActive = true
-  console.log(`🛡   shield -> on`)
-  res.json(state)
-})
-
-app.get('/shield/off', (req, res) => {
-  state.isShieldActive = false
-  console.log(`🛡   shield -> off`)
-  res.json(state)
-})
-
-app.get('/game/new', (req, res) => {
-  state = newGameState()
-  res.json(state)
-})
-
-app.get('/game/start', (req, res) => {
-  state.isGameStarted = true
-  res.json(state)
-})
-
-app.get('/game/lost', (req, res) => {
-  console.log(`☠️   game lost`)
-  state.isGameLost = true
-  res.json(state)
-})
-
-app.get('/game/won', (req, res) => {
-  state.isGameWon = true
-  res.json(state)
-})
-
 io.on('connection', socket => {
-  console.log('⚡️  a user connected')
-  const broadcastEvents = ['move:up', 'move:down', 'move:stop']
-  broadcastEvents.map(event => socket.on(event, () => socket.broadcast.emit(event)))
+  console.log('⚡️ connected')
+  const broadcastEvents = ['move', 'shield', 'weapon', 'state']
+  broadcastEvents.map(event => socket.on(event, data => socket.broadcast.emit(event, data)))
 
   socket.on('disconnect', () => {
-    console.log('🔌  a user disconnected')
+    console.log('🔌  disconnected')
   })
 })
 
@@ -92,5 +27,3 @@ const port = process.env.PORT || 9000
 server.listen(port, () => {
   console.log(`👾  Serving on port ${port}`)
 })
-
-let state = newGameState()
